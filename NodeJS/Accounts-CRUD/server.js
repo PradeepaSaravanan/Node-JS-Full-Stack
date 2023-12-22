cafe86e25fe2b3012530a967268c1ac535212a69
Pradeepa <pradeepa231999@gmail.com>
2023-12-22 16:21:50 +0530

    second commit

--- NodeJS/Accounts-CRUD/server.js
+++ NodeJS/Accounts-CRUD/server.js
@@ -6,12 +6,10 @@ const moment= require('moment')
 require('colors')
 const db= require('./config/db')
 const app=express();
-
 dotenv.config({path:"./config/config.env"});
 db(app);
 app.use(express.json())
 if (process.env.NODE_ENV === "development") app.use(logger("dev"));
-
 app.use("/api/v1", require('./Controller/accounts'));
 module.exports=app
 
